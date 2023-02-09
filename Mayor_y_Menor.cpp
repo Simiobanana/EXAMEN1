ad65cde5555b76b503bf0680a6d5415eb031edd0
Simiobanana <aurvieta28982@ucq.edu.mx>
2023-02-09 16:17:36 -0600

    Update Mayor_y_Menor.cpp

--- Mayor_y_Menor.cpp
+++ Mayor_y_Menor.cpp
@@ -1,17 +1,40 @@
+/*
+	Hacer un programa donde el usuario inserte 3 numeros y se le devuelva el numero mayor y menor
+	4 pasos para su solucion:
+	*Declarar variables
+	*Ingresar los datos
+	*Hacer comparaciones que muestre el resultado
+	*Hacer correr el programa
+*/
 #include <iostream>
 
 using namespace std;
+/**
+ * @version 1.2
+ * @date 09/02/2023
+ * @author Alejandro Urvieta Gonzalez
+ * @title Mayor_y_Menor
+ * @brief El usuario introducira 3 valores para que despues te regrese el balor mayor y el menor
+ */
+ 
+  /**
+ * @brief Paso0: Declaras variables a usar: valor1, valor2, valor3
+ */
+
 float valor1, valor2, valor3;
+
 void IntroduceValores();
 void Comparaciones();
 
-int main()
-{
-	IntroduceValores();
-	Comparaciones();
-	
-}
-
+/**
+ *  @brief Paso1: Pide los 3 valores para determinar cual sera el mayor y cual el menor
+ *  @param valor... guarda los valores que compararà
+ *  @return void
+ *  @code
+    cout<<"introduce el valor 1: ";
+	cin>>valor1;
+    @endcode
+ */
 void IntroduceValores()
 {
 	cout<<"introduce el valor 1: ";
@@ -22,9 +45,12 @@ void IntroduceValores()
 	cin>>valor3;
 }
 
-void Comparaciones()
-{
-	if(valor1>valor2 && valor2>valor3)
+/**
+ *  @brief Paso2: Haces comparaciones para hallar el valor mas grande y el mas chico
+ *  @param valor... guarda el valor ya comparado y lo muestra
+ *  @return void
+ *  @code
+    if(valor1>valor2 && valor2>valor3)
 	{
 		cout<<"el valor mas grande es: "<<valor1<<endl;
 		cout<<"el valor mas chico es: "<<valor3<<endl;
@@ -34,15 +60,29 @@ void Comparaciones()
 		cout<<"el valor mas grande es: "<<valor1<<endl;
 		cout<<"el valor mas chico es: "<<valor2<<endl;
 	}
-	else if(valor1<valor2 && valor2<valor3)
+    @endcode
+ */
+void Comparaciones()
+{
+	if(valor1>valor2 && valor2>valor3)
 	{
-		cout<<"el valor mas grande es: "<<valor3<<endl;
-		cout<<"el valor mas chico es: "<<valor1<<endl;
+		cout<<"el valor mas grande es: "<<valor1<<endl;
+		cout<<"el valor mas chico es: "<<valor3<<endl;
 	}
-	else if(valor3>valor1 && valor2<valor1)
+	else if (valor1>valor3 && valor3>valor2)
+	{
+		cout<<"el valor mas grande es: "<<valor1<<endl;
+		cout<<"el valor mas chico es: "<<valor2<<endl;
+	}
+	else if(valor3>valor2 && valor1>valor2)
 	{
 		cout<<"el valor mas grande es: "<<valor3<<endl;
 		cout<<"el valor mas chico es: "<<valor2<<endl;
+	}
+	else if(valor3>valor2 && valor2>valor1)
+	{
+		cout<<"el valor mas grande es: "<<valor3<<endl;
+		cout<<"el valor mas chico es: "<<valor1<<endl;
 	}
 	else if(valor2>valor1 && valor1>valor3)
 	{
@@ -54,4 +94,14 @@ void Comparaciones()
 		cout<<"el valor mas grande es: "<<valor2<<endl;
 		cout<<"el valor mas chico es: "<<valor1<<endl;
 	}
+}
+
+/**
+ *  @brief Paso3: Haces correr el programa
+ */
+int main()
+{
+	IntroduceValores();
+	Comparaciones();
+	
 }
