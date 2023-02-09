c5c4535bfdfac3ce099a9a00574cf8fa14d35498
Simiobanana <aurvieta28982@ucq.edu.mx>
2023-02-09 15:23:38 -0600

    Update Tu_Edad_Es.cpp

--- Tu_Edad_Es.cpp
+++ Tu_Edad_Es.cpp
@@ -2,21 +2,40 @@
 
 using namespace std;
 
+int valor1, valor2, valor3, edad;
+int AnioActual=2023;
+int DiaActual=9; 
+int MesActual=2;
+
+void AsignacionDeValores();
+void RestaDeValor();
+void CondicionResta();
+void Final();
 int main()
 {
-	int valor1, valor2, valor3, edad;
-	int AnioActual=2023;
-	int DiaActual=9; 
-	int MesActual=2;
-	
+	AsignacionDeValores();
+	RestaDeValor();
+	CondicionResta();
+	Final();
+}
+
+void AsignacionDeValores()
+{
 	cout<<"Ingresa tu numero de dia: ";
 	cin>>valor1;
 	cout<<"ingresa tu numero de mes: ";
 	cin>>valor2;
 	cout<<"Ingresa tu anio de nacimiento: ";
 	cin>>valor3;
-	
+}
+
+void RestaDeValor()
+{
 	edad=AnioActual-valor3;
+}
+
+void CondicionResta()
+{
 	if(valor2==MesActual)
 	{
 		if(valor1>DiaActual)
@@ -28,5 +47,9 @@ int main()
 	{
 		edad=edad-1;
 	}
+}
+
+void Final()
+{
 	cout<<"Tu edad es: "<<edad<<endl;
 }
